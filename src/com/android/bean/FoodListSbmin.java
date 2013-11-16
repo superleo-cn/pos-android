f952ce66d3dee85dc928f9b5cfb24d0306f3827f
hjgang <709872217@qq.com>
2013-11-16 19:30:21 +0800

    菜单打印提交

---
+++ src/com/android/bean/FoodListSbmin.java
@@ -0,0 +1,20 @@
+package com.android.bean;
+
+public class FoodListSbmin {
+
+	private String name;
+	private String values;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getValues() {
+		return values;
+	}
+	public void setValues(String values) {
+		this.values = values;
+	}
+	
+}
