ef57c6885234a8b01db401da8348c4c68a5f146b
LEO HU <superleo.peking@gmail.com>
2013-11-21 16:57:52 +0800

    Constants 修改

--- src/com/android/common/Constants.java
+++ src/com/android/common/Constants.java
@@ -76,7 +76,7 @@ public final class Constants {
 	/** 与服务器端连接的协议名 */
 	public static final String PROTOCOL = "http://";
 	/** 服务器IP */
-	public static final String HOST = "http://ec2-54-254-145-129.ap-southeast-1.compute.amazonaws.com";
+	public static final String HOST = "ec2-54-254-145-129.ap-southeast-1.compute.amazonaws.com";
 	/** 服务器端口号 */
 	public static final String PORT = ":8080";
 	/** 应用上下文名 */
