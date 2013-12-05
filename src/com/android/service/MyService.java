74d6cc869d83a9aed244b46669b7c3d91dc833f3
LEO HU <superleo.peking@gmail.com>
2013-12-05 17:15:41 +0800

    登陆黑屏。

--- src/com/android/service/MyService.java
+++ src/com/android/service/MyService.java
@@ -13,6 +13,7 @@ import java.util.Map;
 import android.app.Service;
 import android.app.WallpaperManager;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -37,20 +38,42 @@ public class MyService extends Service {
     @Override  
     public void onStart(Intent intent,int startId)  
     {  
-    	 /** 判断今天是否是最新的*/
-		SimpleDateFormat df=new SimpleDateFormat("yyyy-MM-dd");
-    	String date=df.format(new Date());
-    	MyService.this.search_date=date;
-    	
-    	if(!isLatestData()){
-    		post_diancai();
-    		post_payList();
-	    	post_numList(); 
-	    	post_dailyMoney();
-    	}
-    	System.out.println("闹钟执行中。。。");
+    	new UpdateOperation().execute("");
         super.onStart(intent, startId);  
     }  
+    
+    private class UpdateOperation extends AsyncTask<String, Void, Integer> {
+
+        @Override
+        protected Integer doInBackground(String... objs) {
+        	/** 判断今天是否是最新的*/
+    		SimpleDateFormat df=new SimpleDateFormat("yyyy-MM-dd");
+        	String date=df.format(new Date());
+        	MyService.this.search_date=date;
+        	
+        	if(!isLatestData()){
+        		post_diancai();
+        		post_payList();
+    	    	post_numList(); 
+    	    	post_dailyMoney();
+        	}
+        	System.out.println("闹钟执行中。。。");
+        	return 1;
+        }        
+
+        @Override
+        protected void onPostExecute(Integer result) {   
+        }
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+        }
+    }
+    
     @Override  
     public void onCreate()  
     {  
