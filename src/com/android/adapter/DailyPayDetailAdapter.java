66e78ae7601c3fb23f27c4fd754be331e8f56206
hjgang <709872217@qq.com>
2013-12-10 11:46:40 +0800

    i----

--- src/com/android/adapter/DailyPayDetailAdapter.java
+++ src/com/android/adapter/DailyPayDetailAdapter.java
@@ -1,5 +1,6 @@
 package com.android.adapter;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 import android.content.Context;
@@ -81,7 +82,6 @@ public class DailyPayDetailAdapter extends BaseAdapter {
 
 				@Override
 				public boolean onTouch(View view, MotionEvent event) {
-					// TODO Auto-generated method stub
 					 if(event.getAction() == MotionEvent.ACTION_UP) {
 						 
 	                     index= position;
@@ -139,7 +139,7 @@ public class DailyPayDetailAdapter extends BaseAdapter {
 
 		bean = classList.get(position);
 		viewHolder.text_id_name.setText(bean.getName());
-		viewHolder.text_id_price.setText(bean.getPrice());
+		viewHolder.text_id_price.setText(new DecimalFormat("0.00").format(Double.parseDouble(bean.getPrice())).toString());
 		
 		
 		
