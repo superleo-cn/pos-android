eb905373299fd4b0b7d767e989cf12e98f542b06
LEO HU <superleo.peking@gmail.com>
2013-12-28 01:43:44 +0800

    修改登录退出日志错误。

--- src/com/android/activity/BasicActivity.java
+++ src/com/android/activity/BasicActivity.java
@@ -1,28 +1,24 @@
 package com.android.activity;
 
-import android.app.Dialog;
-import android.content.Context;
-
 import com.android.R;
+import com.android.component.ActivityComponent;
 import com.android.component.StringResComponent;
 import com.android.component.ui.MenuComponent;
-import com.android.dialog.ConfirmDialog;
 import com.googlecode.androidannotations.annotations.Bean;
 import com.googlecode.androidannotations.annotations.Click;
 import com.googlecode.androidannotations.annotations.EActivity;
-import com.googlecode.androidannotations.annotations.RootContext;
 
 @EActivity
 public abstract class BasicActivity extends AbstractActivity {
-
-	@RootContext
-	Context context;
 
 	@Bean
 	public MenuComponent menuComponent;
 
 	@Bean
 	StringResComponent stringResComponent;
+
+	@Bean
+	ActivityComponent activityComponent;
 
 	/**
 	 * @TODO: 最后放到基类里面去
@@ -31,27 +27,6 @@ public abstract class BasicActivity extends AbstractActivity {
 	@Click(R.id.menu_btn)
 	public void menu() {
 		menuComponent.initPopupWindow();
-	}
-
-	@Click(R.id.layout_exit)
-	void layoutExitOnClick() {
-		buildExitDialog().show();
-	}
-
-	/**
-	 * 退出系统操作
-	 * 
-	 * @return
-	 */
-	public Dialog buildExitDialog() {
-		return new ConfirmDialog(context, stringResComponent.messageTitle, stringResComponent.messageExit) {
-
-			@Override
-			public void doClick() {
-				loginComponent.executeLogout(myApp.getUserId(), myApp.getShopId());
-			}
-
-		}.build();
 	}
 
 }
