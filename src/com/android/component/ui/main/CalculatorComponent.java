da674cf688f64283937deb1fd76d0939ae60cd1c
LEO HU <superleo.peking@gmail.com>
2014-01-21 23:20:27 +0800

    Clear 修改

--- src/com/android/component/ui/main/CalculatorComponent.java
+++ src/com/android/component/ui/main/CalculatorComponent.java
@@ -104,7 +104,10 @@ public class CalculatorComponent {
 			calulation(sbuff, ".", orderComponent.getGathering());
 			break;
 		case 11:
-			orderComponent.clean();
+			int sb_length = sbuff.length();
+			sbuff.delete(0, sb_length);
+			orderComponent.getGathering().setText("0.00");
+			orderComponent.getSurplus().setText("0.00");
 			break;
 		}
 
