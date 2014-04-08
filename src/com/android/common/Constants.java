5b0d6856ad1ea45fd630a6d2ff2267f61cafc723
LEO HU <superleo.peking@gmail.com>
2014-04-08 21:01:04 +0800

    add new interface for food.

--- src/com/android/common/Constants.java
+++ src/com/android/common/Constants.java
@@ -54,7 +54,7 @@ public final class Constants {
 	public static final String PROTOCOL = "http://";
 	/** 服务器IP */
 	public static final String HOST =  "ec2-54-254-145-129.ap-southeast-1.compute.amazonaws.com";
-	//public static final String HOST  = "192.168.0.123";
+	//public static final String HOST  = "192.168.0.14";
 	//public static final String HOST = "localhost";
 	/** 服务器端口号 */
 	public static final String PORT = ":8080";
@@ -69,7 +69,7 @@ public final class Constants {
 	/** 店铺完整URL路径 */
 	public static final String URL_SHOP_PATH = URL_CONTEXTPATH + "/shops/listJson/";
 	/** 点菜单完整URL路径 */
-	public static final String URL_FOODSLIST_PATH = URL_CONTEXTPATH + "/foods/listJson/";
+	public static final String URL_FOODSLIST_PATH = URL_CONTEXTPATH + "/foods/listExtJson/";
 	/** 支付款项 */
 	public static final String URL_PAY_DETAIL = URL_CONTEXTPATH + "/consumptions/listJson/";
 	/** 带回总数 */
