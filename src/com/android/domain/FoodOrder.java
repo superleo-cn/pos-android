b5303e686326dce7b436c784401cd55e2bbc3b78
wlh1980 <wanglihui@wangs-MacBook-Air.local>
2014-12-18 22:45:15 +0800

    fdsaf

--- src/com/android/domain/FoodOrder.java
+++ src/com/android/domain/FoodOrder.java
@@ -1,6 +1,5 @@
 package com.android.domain;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -29,9 +28,6 @@ public class FoodOrder extends Model {
 
 	@Column(name = "shop_id")
 	public String shopId;
-	
-	@Column(name = "table_id")
-	public String tableId;
 
 	@Column(name = "retail_price")
 	public Double retailPrice;
@@ -65,27 +61,26 @@ public class FoodOrder extends Model {
 
 	@Column(name = "attributesContext")
 	public String attributesContext;
-	
+
 	@Column(name = "flag")
 	public String flag;
-	
+
 	@Column(name = "gst_charge")
 	public Double gstCharge;
-	
+
 	@Column(name = "service_charge")
 	public Double serviceCharge;
 
+	@Column(name = "packageable")
+	public String packageable;
+
 	@Override
 	public String toString() {
-		return "FoodOrder [orderId=" + orderId + ", userId=" + userId
-				+ ", shopId=" + shopId + ", tableId=" + tableId + ", retailPrice=" + retailPrice
-				+ ", quantity=" + quantity + ", foodId=" + foodId
-				+ ", discount=" + discount + ", totalPackage=" + totalPackage
-				+ ", foc=" + foc + ", orderType=" + orderType + ", status="
-				+ status + ", date=" + date + ", attributesID=" + attributesID
-				+ ", attributesContext=" + attributesContext + ", flag=" + flag
-				+ ", gstCharge=" + gstCharge + ", serviceCharge="
-				+ serviceCharge + "]";
+		return "FoodOrder [orderId=" + orderId + ", userId=" + userId + ", shopId=" + shopId + ", retailPrice=" + retailPrice
+				+ ", quantity=" + quantity + ", foodId=" + foodId + ", discount=" + discount + ", totalPackage=" + totalPackage + ", foc="
+				+ foc + ", orderType=" + orderType + ", status=" + status + ", date=" + date + ", attributesID=" + attributesID
+				+ ", attributesContext=" + attributesContext + ", flag=" + flag + ", gstCharge=" + gstCharge + ", serviceCharge="
+				+ serviceCharge + ", packageable=" + packageable + "]";
 	}
 
 	/**
@@ -104,7 +99,7 @@ public class FoodOrder extends Model {
 	 * @return
 	 */
 	public static List<FoodOrder> queryListByStatus(String status, int size) {
-		return new Select().from(FoodOrder.class).where("status = ?", status).limit("0, " + size).execute();
+		return new Select().from(FoodOrder.class).where("status = ? ", status).limit("0, " + size).execute();
 	}
 
 	/**
@@ -138,20 +133,17 @@ public class FoodOrder extends Model {
 	 * @param myApp
 	 * @param is_foc
 	 */
-	public static void save(SelectFoodBean bean, MyApp myApp, boolean is_foc, String orderType, String tableId, String flag) {
+	public static void save(SelectFoodBean bean, MyApp myApp, boolean is_foc, String orderType) {
 		if (StringUtils.isNotEmpty(bean.getFood_id()) && !StringUtils.equals(bean.getFood_id(), "0")) {
 			FoodOrder food_order = new FoodOrder();
 			food_order.status = Constants.DB_FAILED;// 是否成功 1是 0否
 			food_order.shopId = myApp.getShopId();// 店idmyApp.getShopid()
-			if(StringUtils.isNotEmpty(tableId.trim())){
-				food_order.orderId = tableId;
-			}else{
-				food_order.orderId = SystemHelper.getUuid();
-			}
+			food_order.orderId = SystemHelper.getUuid();
 			food_order.totalPackage = String.valueOf(bean.getDabao_price());// 打包钱数
 			food_order.discount = String.valueOf(bean.getDazhe_price()); // 打折钱数
 			food_order.userId = myApp.getUserId();// 用户id
-			double totalRetailPrice = Double.parseDouble(bean.getFood_price()) * Integer.parseInt(bean.getFood_num()) - bean.getDazhe_price() + bean.getDabao_price();
+			double totalRetailPrice = Double.parseDouble(bean.getFood_price()) * Integer.parseInt(bean.getFood_num())
+					- bean.getDazhe_price() + bean.getDabao_price();
 			if (is_foc) {
 				food_order.foc = Constants.FOC_YES;// 是否免费 1是 0否
 				totalRetailPrice = Constants.DEFAULT_PRICE_NUM_INT;
@@ -164,7 +156,6 @@ public class FoodOrder extends Model {
 			food_order.foodId = bean.getFood_id();// 食物id
 			food_order.quantity = bean.getFood_num();// 数量
 			food_order.orderType = orderType;
-			food_order.flag = flag;
 			food_order.attributesID = bean.getAttributesID();// 属性ID
 			food_order.attributesContext = bean.getAttributesContext();// 属性名字
 			food_order.date = DateUtils.dateToStr(new Date(), DateUtils.YYYY_MM_DD_HH_MM_SS);
@@ -205,54 +196,5 @@ public class FoodOrder extends Model {
 	 */
 	public static void deleteByDate(String time) {
 		new Delete().from(FoodOrder.class).where("date < ?", time).execute();
-	}
-	
-	/**
-	 * 查询输入的桌号是否存在
-	 * 
-	 * @param orderNo 订单号
-	 */
-	public static boolean queryOrderIdExist(String orderId) {
-		boolean isOrderIdExist = false;
-		List<FoodOrder> foodOrderList =  new Select().from(FoodOrder.class).where("order_id = ?", orderId).execute();
-		if(foodOrderList.size() > 0)
-			isOrderIdExist = true;
-		return isOrderIdExist;
-	}
-	
-	/**
-	 * 根据订单号删除订单信息
-	 * 
-	 * @param orderNo 订单号
-	 */
-	public static void deleteOrderByOrderId(String orderId) {
-		new Delete().from(FoodOrder.class).where("order_id = ?", orderId).execute();
-	}
-	
-	/**
-	 * 查询所有挂单状态且不重复的桌号
-	 * 
-	 */
-	public static List<String> queryOrderListDistact() {
-		List<FoodOrder> foodOrderList = new Select().from(FoodOrder.class).where("flag = ?", Constants.FOODORDER_PAUSE)
-				.groupBy("order_id")
-				.orderBy("date")
-				.orderBy("order_id").execute();
-		List<String> orders = new ArrayList<String>();
-		for (int i = 0; i < foodOrderList.size(); i++) {
-			FoodOrder foodOrder = foodOrderList.get(i);
-			orders.add(foodOrder.orderId);
-		}
-		return orders;
-	}
-	
-	/**
-	 * 根据桌号查询订单数据
-	 * 
-	 * 
-	 * @param orderNo 订单号
-	 */
-	public static List<FoodOrder> queryListByOrderId(String tableId) {
-		return new Select().from(FoodOrder.class).where("order_id = ?", tableId).execute();
 	}
 }
